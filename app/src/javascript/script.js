3766263101184937ecdce540f35dbe9c20a80fd3
AndrUsM <oksud22@gmail.com>
2019-11-16 08:53:12 +0200

    fixed

--- app/src/javascript/script.js
+++ app/src/javascript/script.js
@@ -2,23 +2,19 @@ if(document.body.clientWidth < 450){
     // document.querySelectorAll('.page-header_nav_list_item').forEach(list_item => {
     //     list_item.style.display = 'none'
     // })
-    document.querySelector('.page-header_nav-toggler').addEventListener('click', (e) => {
+    document.querySelector('.page-header .nav-panel__burger').addEventListener('click', (e) => {
         // e.preventDefault()
-        if(document.querySelector('.page-header_nav_list_item').style.display == 'none' || !document.querySelector('.page-header_nav_list_item').style.display){
-            document.querySelectorAll('.page-header_nav_list_item_link').forEach(a => {
-                a.style.display = 'block'
-            })
-            document.querySelectorAll('.page-header_nav_list_item').forEach(list_item => {
-                list_item.style.display = 'block'
-            })
+        if(document.querySelector('.page-header .nav-panel__list').style.display == 'none' || document.querySelector('.page-header .nav-panel__list').style.display == undefined){
+            document.querySelector('.page-header .nav-panel__list').style.display = 'block'
+            // document.querySelectorAll('.page-header .nav-panel__list').forEach(list_item => {
+            //     list_item.style.display = 'block'
+            // })
         }
         else{
-            document.querySelectorAll('.page-header_nav_list_item').forEach(list_item => {
-                list_item.style.display = 'none'
-            })
-            document.querySelectorAll('.page-header_nav_list_item_link').forEach(a => {
-                a.style.display = 'none'
-            })
+            document.querySelector('.page-header .nav-panel__list').style.display = 'none'
+            // document.querySelectorAll('.page-header .nav-panel__list').forEach(a => {
+            //     a.style.display = 'none'
+            // })
         }
     })
 }
